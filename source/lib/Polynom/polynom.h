4569da7fc2ada5e4274a7d136c64e0fe41ffddf5
xsevios <nikitalomov@list.ru>
2016-03-08 15:59:43 +0000

    Подготовка рабочего пространства (Ломов и Кривицкий)

--- source/lib/Polynom/polynom.h
+++ source/lib/Polynom/polynom.h
@@ -18,12 +18,25 @@ public:
     
     void setDim(int dim);
     void setCoef(vector<double> coef);
+    void initPolynom(int dimGF, vector<double> coef, int irreducible);
     void setIrreducible(bool);
     
     vector<double> getCoef();
     int getDim();
     bool isIrreducible();
     
+    Polynom& operator=(Polynom p);
+
+    Polynom operator+(const Polynom& p);
+    Polynom operator-(const Polynom& p);
+    Polynom operator*(const Polynom& p);
+    Polynom operator/(const Polynom& p);
+    
+    friend ostream& operator<<(ostream& os, const Polynom& p);
+    friend istream& operator>>(istream& is, Polynom& p);
+    
+    bool operator==(const Polynom& p);
+    bool operator!=(const Polynom& p);
 };
 
 #endif
