099cb34c6b2ee0d8eb4f85324d3d57fd87ca2670
xsevios <nikitalomov@list.ru>
2016-03-09 18:44:42 +0000

    Добавлен класс проверки многочленов (Ломов и Кривицкий)

---
+++ source/lib/Polynom/checkIrreducible.h
@@ -0,0 +1,13 @@
+#ifndef _CHECK_IRREDUCIBLE_H
+#define _CHECK_IRREDUCIBLE_H
+
+#include "polynom.h"
+
+class PolynomChecker
+{
+public:
+    
+    void operator()(Polynom* polynom);
+};
+
+#endif
--- source/lib/Polynom/polynom.h
+++ source/lib/Polynom/polynom.h
@@ -10,20 +10,24 @@ class Polynom
 {
     int dimGF;
     vector<double> coef;
+    
+    /*
+     * Переменная содержит информацию о приводимости многочлена в следующем виде:
+     * -1 - еще не проверен (не является опорной точкой для построения алгоритма проверки)
+     *  0 - неприводимый
+     *  1 - приводимый
+     */
     int irreducible;
     
 public:
     Polynom(int dimGF, vector<double> coef);
     ~Polynom();
     
-    void setDim(int dim);
-    void setCoef(vector<double> coef);
-    void initPolynom(int dimGF, vector<double> coef, int irreducible);
     void setIrreducible(bool);
     
     vector<double> getCoef();
     int getDim();
-    bool isIrreducible();
+    int isIrreducible();
     
     Polynom& operator=(Polynom p);
 
@@ -32,8 +36,8 @@ public:
     Polynom operator*(const Polynom& p);
     Polynom operator/(const Polynom& p);
     
-    friend ostream& operator<<(ostream& os, const Polynom& p);
-    friend istream& operator>>(istream& is, Polynom& p);
+    friend ostream& operator<<(ostream& out, const Polynom& p);
+    friend istream& operator>>(istream& in, Polynom& p);
     
     bool operator==(const Polynom& p);
     bool operator!=(const Polynom& p);
