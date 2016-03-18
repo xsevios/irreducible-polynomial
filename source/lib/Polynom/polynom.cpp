cbc35884312451a49c09c5a006cce1bda09ee470
saartem <sapbufer1@gmail.com>
2016-03-18 20:25:08 +0000

    Определение почти всех функций класса Polynom (Бритвин, Ловков, Фисенко, Соболев, Пугаченко)

--- source/lib/Polynom/polynom.cpp
+++ source/lib/Polynom/polynom.cpp
@@ -1,6 +1,7 @@
 #include "polynom.h"
 #include <iostream>
 #include <string>
+#include <sstream>
 
 extern "C" Polynom* create_polynom(std::string strPolynom)
 {
@@ -12,38 +13,136 @@ extern "C" void destroy_polynom(Polynom* object)
     delete object;
 }
 
-Polynom::Polynom(int dim, vector<double> coef)
+Polynom::Polynom(int dim, vector<int> coef)
 {
     this->dimGF = dim;
     this->coef = coef;
+    this->irreducible = NEED_CHECK;
 }
 
 Polynom::Polynom(string strPolynom)
 {
-    irreducible = -1;
+    irreducible = NEED_CHECK;
+    istringstream iss(strPolynom);
+    
+    string sub;
+    iss >> sub;
+    dimGF = stoi(sub);
+    
+    while(iss)
+    {
+        iss >> sub;
+        coef.push_back(stoi(sub));
+    }
 }
 
-Polynom::~Polynom()
-{
-}
+Polynom::~Polynom(){} // don't change
 
 int Polynom::getDim() const
 {
     return dimGF;
 }
 
-vector<double> Polynom::getCoef() const
+vector<int> Polynom::getCoef() const
 {
     return coef;
 }
 
-int Polynom::isIrreducible() const
+PolynomState Polynom::isIrreducible() const
 {
     return irreducible;
 }
 
-
-ostream& operator<<(ostream& out, const Polynom& p)
+void Polynom::setIrreducible(PolynomState state)
 {
-    return out;
+   irreducible = state;
+}
+
+Polynom& Polynom::operator=(const Polynom& p)
+{
+    dimGF = p.dimGF;
+    irreducible = p.irreducible;
+    coef = p.coef;
+    
+    return *this;
+}
+
+Polynom Polynom::operator+(const Polynom& p)
+{
+    int new_dim = dimGF;
+    
+    int count = coef.size() > p.coef.size() ? coef.size() : p.coef.size();
+    
+    vector<int> new_coef;
+    new_coef.resize(count);
+    for(unsigned i = 0; i < new_coef.size(); i++)
+    {
+        new_coef[i] = 0;
+        
+        if (i < coef.size())
+            new_coef[i] += coef[i];
+            
+        if (i < p.coef.size())
+            new_coef[i] += p.coef[i];
+        
+        new_coef[i] %= new_dim;
+    }
+    
+    Polynom result(new_dim, new_coef);
+    return result;
+}
+
+Polynom Polynom::operator-(const Polynom& p)
+{
+    int new_dim = dimGF;
+    unsigned count = coef.size() > p.coef.size() ? coef.size() : p.coef.size();
+    
+    vector<int> new_coef;
+    new_coef.resize(count);
+    
+    for(unsigned i = 0; i < count; i++)
+    {
+        new_coef[i] = 0;
+        
+        if (i < coef.size())
+            new_coef[i] += coef[i];
+            
+        if (i < p.coef.size())
+            new_coef[i] -= p.coef[i];
+            
+        if(new_coef[i] < 0)
+            new_coef[i] += new_dim;
+    }
+    
+    Polynom result(new_dim, new_coef);
+    return result;
+}
+
+Polynom Polynom::operator*(const Polynom& p)
+{
+    // TODO
+}
+
+Polynom Polynom::operator/(const Polynom& p)
+{
+    // TODO
+}
+
+bool Polynom::operator==(const Polynom& p)
+{
+    unsigned size = coef.size();
+
+    if (size != p.coef.size())
+        return false;
+    else
+        for(unsigned i = 0; i < size; i++)
+            if (coef[i] != p.coef[i]) 
+                return false;
+    
+    return true;
+}
+
+bool Polynom::operator!=(const Polynom& p)
+{
+    return !operator==(p);
 }
--- source/lib/Polynom/polynom.h
+++ source/lib/Polynom/polynom.h
@@ -5,31 +5,32 @@
 #include <stdio.h>
 #include <vector>
 #include <string>
+
 using namespace std;
+
+enum PolynomState
+{
+    NEED_CHECK  = -1,
+    REDUCIBLE   = 0,
+    IRREDUCIBLE = 1
+};
 
 class Polynom
 {
     int dimGF;
-    vector<double> coef;
-    
-    /*
-     * Переменная содержит информацию о приводимости многочлена в следующем виде:
-     * -1 - еще не проверен (не является опорной точкой для построения алгоритма проверки)
-     *  0 - неприводимый
-     *  1 - приводимый
-     */
-    int irreducible;
+    vector<int> coef;
+    PolynomState irreducible;
     
 public:
-    Polynom(int dimGF, vector<double> coef);
+    Polynom(int dimGF, vector<int> coef);
     Polynom(string strPolynom);
     virtual ~Polynom();
     
-    void setIrreducible(bool);
+    void setIrreducible(PolynomState);
     
-    virtual vector<double> getCoef() const;
+    virtual vector<int> getCoef() const;
     virtual int getDim() const;
-    virtual int isIrreducible() const;
+    virtual PolynomState isIrreducible() const;
     
     Polynom& operator=(const Polynom& p);
 
@@ -37,9 +38,6 @@ public:
     Polynom operator-(const Polynom& p);
     Polynom operator*(const Polynom& p);
     Polynom operator/(const Polynom& p);
-    
-    friend ostream& operator<<(ostream& out, const Polynom& p);
-    friend istream& operator>>(istream& in, Polynom& p);
     
     bool operator==(const Polynom& p);
     bool operator!=(const Polynom& p);
