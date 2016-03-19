fe60de1ae66c74264558fa7aabedbf7a60bd80df
xsevios <nikitalomov@list.ru>
2016-03-19 11:42:33 +0000

    Изменен подход к вызову приложения (считывание окружения из параметров командной строки) (Ломов и Кривицкий)

--- source/driver/main.cpp
+++ source/driver/main.cpp
@@ -1,10 +1,55 @@
 #include "driver.h"
+#include <iostream>
+#include <string.h>
 
 using namespace std;
 
-int main()
+#define _CONFIG "environ.conf"
+#define _LIB "libirredpoly.so"
+
+void usage(const char *prog)
 {
-    Driver driver("libirredpoly.so", "workSpace.cfg");
+    cout << "Usage:" << endl << prog << " [<options>]" << endl <<
+    "    -c config_file           use config_file as configuration file" << endl <<
+    "    -l library_file          use library_file as library file" << endl;
+}
+
+int main(int argc, char **argv)
+{
+    char const* conf_path = _CONFIG;
+    char const* lib_path = _LIB;
+    
+    int c = 1;
+    while ( c < argc )
+    {
+        if (strcmp(argv[c], "-c") == 0)
+        {
+            if (++c >= argc)
+            {
+                cout << "Runtime-Error: -c option requires an input argument" << endl;
+                usage(argv[0]);
+                return 1;
+            }
+            else
+                conf_path = argv[c];
+        }
+        
+        if (strcmp(argv[c], "-l") == 0)
+        {
+            if (++c >= argc)
+            {
+                cout << "Runtime-Error: -l option requires an input argument" << endl;
+                usage(argv[0]);
+                return 1;
+            }
+            else
+                lib_path = argv[c];
+        }
+        
+        c++;
+    }
+    
+    Driver driver(lib_path, conf_path);
     
     driver.loadLibrary();
     driver.readConfig();
