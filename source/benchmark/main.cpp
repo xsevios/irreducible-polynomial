1826c189e3bbcf349e57c149deef0b79f533779b
Nikita Lomov <nikitalom@gmail.com>
2024-07-04 18:06:59 +0300

    Benchmark for prepared polynoms

--- source/benchmark/main.cpp
+++ source/benchmark/main.cpp
@@ -10,17 +10,41 @@ using namespace std;
 void usage(const char *prog)
 {
     cout << "Usage:" << endl << prog << " [<options>]" << endl <<
+    "    -p polynoms_file         use prepared polynoms from polynoms_file" << endl <<
     "    -c config_file           use config_file as configuration file" << endl <<
     "    -l library_file          use library_file as library file" << endl;
 }
 
+void printProgress(int currentBenchmark, int totalBenchmarks, int barWidth = 70)
+{
+    float progress = (float) currentBenchmark / totalBenchmarks;
+
+    std::cout << "[";
+    int      pos = barWidth * progress;
+    for (int i   = 0; i < barWidth; ++i)
+    {
+        if (i < pos) std::cout << "=";
+        else if (i == pos) std::cout << ">";
+        else std::cout << " ";
+    }
+    std::cout << "] " << int(progress * 100.0) << " % (" << currentBenchmark << "/" << totalBenchmarks << ")\r";
+    std::cout.flush();
+}
+
+int get_result_ms(std::vector<Benchmark>& results, int index)
+{
+    return (int) std::chrono::duration_cast<std::chrono::milliseconds>(std::get<3>(results[index])).count();
+}
+
+// -p /mnt/d/Development/Projects/irreducible-polynomial/pari/polynoms.txt
 int main(int argc, char **argv)
 {
     char const* conf_path = _CONFIG;
     char const* lib_path = _LIB;
+    char const* poly_path = nullptr;
     
     int c = 1;
-    while ( c < argc )
+    while (c < argc)
     {
         if (strcmp(argv[c], "-c") == 0)
         {
@@ -45,35 +69,30 @@ int main(int argc, char **argv)
             else
                 lib_path = argv[c];
         }
+
+        if (strcmp(argv[c], "-p") == 0)
+        {
+            if (++c >= argc)
+            {
+                cout << "Runtime-Error: -p option requires an input argument" << endl;
+                usage(argv[0]);
+                return 1;
+            }
+            else
+                poly_path = argv[c];
+        }
         
         c++;
     }
 
     BenchmarkRunner benchmark(lib_path);
 
-//    for (auto i = 2; i < 1100; i++)
-//    {
-//        if (isPrime(i))
-//        {
-//            bigint two = 2;
-//            bigint limit = two ^ (bigint)63;
-//
-//            bigint cur = i;
-//
-//            bigint q = 63;
-//            while ((cur ^ q) > limit)
-//                q = q - 1;
-//
-//            std::cout << "{" << i << ", " << q << "}," << std::endl;
-//        }
-//    }
-
     benchmark.loadLibrary();
 
-    int minDim              = 11;
-    int maxDim              = 19;
-    int minDegree           = 10;
-    int maxDegree           = 15;
+    int minDim              = 1;
+    int maxDim              = 1000;
+    int minDegree           = 1;
+    int maxDegree           = 50;
     int polyCountForEach    = 1000;
     int totalBenchmarks     = (maxDim - minDim) * (maxDegree - minDegree);
     int currentBenchmark    = 0;
@@ -89,77 +108,102 @@ int main(int argc, char **argv)
     int cantorzassenhausTotalTimeMs = 0;
     int rabinTotalTimeMs            = 0;
 
-    for (int dim = minDim; dim < maxDim; dim++)
+    if (poly_path == nullptr)
     {
-        if (isPrime(dim))
+        for (int dim = minDim; dim < maxDim; dim++)
         {
-            for (int deg = minDegree; deg < maxDegree; deg += 1)
+            if (isPrime(dim))
             {
-                std::vector<Polynom> polynoms = BenchmarkRunner::generatePolynoms(dim, dim, deg, deg, polyCountForEach);
-                auto results = benchmark.runAll(polynoms);
-
-                for (auto& result : results)
+                for (int deg = minDegree; deg < maxDegree; deg += 1)
                 {
-                    auto duration = std::get<3>(result);
-                    duration.count();
+                    std::vector<Polynom> polynoms = BenchmarkRunner::generatePolynoms(dim, dim, deg, deg,
+                                                                                      polyCountForEach);
+                    auto                 results  = benchmark.runAll(polynoms);
+
+                    auto berlekampMs        = get_result_ms(results, 0);
+                    auto cantorzassenhausMs = get_result_ms(results, 1);
+                    auto rabinMs            = get_result_ms(results, 2);
+
+                    berlekampTotalTimeMs += berlekampMs;
+                    cantorzassenhausTotalTimeMs += cantorzassenhausMs;
+                    rabinTotalTimeMs += rabinMs;
+
+                    berlekampFile << berlekampMs << " ";
+                    cantorzassenhaus << cantorzassenhausMs << " ";
+                    rabin << rabinMs << " ";
+
+                    currentBenchmark++;
+                    printProgress(currentBenchmark, totalBenchmarks);
                 }
 
-                auto berlekampMs        = (int)std::chrono::duration_cast<std::chrono::milliseconds>(std::get<3>(results[0])).count();
-                auto cantorzassenhausMs = (int)std::chrono::duration_cast<std::chrono::milliseconds>(std::get<3>(results[1])).count();
-                auto rabinMs            = (int)std::chrono::duration_cast<std::chrono::milliseconds>(std::get<3>(results[2])).count();
-
-                berlekampTotalTimeMs        += berlekampMs;
-                cantorzassenhausTotalTimeMs += cantorzassenhausMs;
-                rabinTotalTimeMs            += rabinMs;
-
-//                berlekampFile       << deg << " " << berlekampMs << " ";
-//                cantorzassenhaus    << deg << " " << cantorzassenhausMs << " ";
-//                rabin               << deg << " " << rabinMs << " ";
-//
-//                berlekampFile << std::endl;
-//                cantorzassenhaus << std::endl;
-//                rabin << std::endl;
-
-                berlekampFile       << berlekampMs << " ";
-                cantorzassenhaus    << cantorzassenhausMs << " ";
-                rabin               << rabinMs << " ";
-
-                currentBenchmark++;
-                progress = (float)currentBenchmark / totalBenchmarks;
-
-                std::cout << "[";
-                int pos = barWidth * progress;
-                for (int i = 0; i < barWidth; ++i)
-                {
-                    if (i < pos) std::cout << "=";
-                    else if (i == pos) std::cout << ">";
-                    else std::cout << " ";
-                }
-                std::cout << "] " << int(progress * 100.0) << " %\r";
-                std::cout.flush();
+                berlekampFile << std::endl;
+                cantorzassenhaus << std::endl;
+                rabin << std::endl;
             }
-
-            berlekampFile << std::endl;
-            cantorzassenhaus << std::endl;
-            rabin << std::endl;
-        }
-        else
-        {
-            currentBenchmark += maxDegree - minDegree;
-            progress = (float)currentBenchmark / totalBenchmarks;
-
-            std::cout << "[";
-            int pos = barWidth * progress;
-            for (int i = 0; i < barWidth; ++i)
+            else
             {
-                if (i < pos) std::cout << "=";
-                else if (i == pos) std::cout << ">";
-                else std::cout << " ";
+                currentBenchmark += maxDegree - minDegree;
+                printProgress(currentBenchmark, totalBenchmarks);
             }
-            std::cout << "] " << int(progress * 100.0) << " %\r";
-            std::cout.flush();
         }
     }
+    else
+    {
+        ifstream polysFile(poly_path, ios::in);
+
+        std::string line;
+        std::getline(polysFile, line);
+
+        std::istringstream iss(line);
+        iss >> polyCountForEach;
+
+        std::vector<Polynom> polynoms;
+        while (std::getline(polysFile, line))
+        {
+            Polynom p(line);
+            polynoms.push_back(p);
+        }
+        totalBenchmarks = polynoms.size();
+
+        std::vector<Polynom> polynomsBuf;
+        int nCurDim = polynoms[0].GetField()->GetDim();
+        for (auto& p : polynoms)
+        {
+            if (p.GetField()->GetDim() > nCurDim)
+            {
+                nCurDim = p.GetField()->GetDim();
+                berlekampFile << std::endl;
+                cantorzassenhaus << std::endl;
+                rabin << std::endl;
+            }
+
+            polynomsBuf.push_back(p);
+
+            if (polynomsBuf.size() == polyCountForEach)
+            {
+                auto results  = benchmark.runAll(polynomsBuf);
+
+                auto berlekampMs        = get_result_ms(results, 0);
+                auto cantorzassenhausMs = get_result_ms(results, 1);
+                auto rabinMs            = get_result_ms(results, 2);
+
+                berlekampTotalTimeMs += berlekampMs;
+                cantorzassenhausTotalTimeMs += cantorzassenhausMs;
+                rabinTotalTimeMs += rabinMs;
+
+                berlekampFile << berlekampMs << " ";
+                cantorzassenhaus << cantorzassenhausMs << " ";
+                rabin << rabinMs << " ";
+
+                currentBenchmark += polyCountForEach;
+                printProgress(currentBenchmark, totalBenchmarks);
+
+                polynomsBuf.clear();
+            }
+        }
+    }
+
+
     std::cout << std::endl;
 
     std::cout << "Berlakamp total ms: "         << berlekampTotalTimeMs         << std::endl;
