797d1b09fa86859493477c685e8fc0c2fcad34f7
Nikita Lomov <nikitalom@gmail.com>
2025-01-14 03:42:33 +0300

    Remove old polynomial implementation from lib

--- source/lib/lib.h
+++ source/lib/lib.h
@@ -1,7 +1,6 @@
 #ifndef _LIBRARYHEADERS_H
 #define _LIBRARYHEADERS_H
 
-#include "Polynom/polynom.h"
 #include "Polynom/polynom_ext.h"
 #include "Polynom/checkIrreducible.h"
 #include "Scheduler/scheduler.h"
